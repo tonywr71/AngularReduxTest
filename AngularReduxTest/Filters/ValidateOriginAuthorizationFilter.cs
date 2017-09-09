fc31a75c1bcff35b80464e28f702c975ebb0c9dc
Tony Wright <tony@beyondthegrid.com.au>
2017-09-09 16:51:31 +1000

    added antiforgery

---
+++ AngularReduxTest/Filters/ValidateOriginAuthorizationFilter.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AngularReduxTest.Filters
+{
+  public class ValidateOriginAuthorizationFilter : IAuthorizationFilter
+  {
+    private IOptions<ValidateOriginOptions> options;
+
+    public ValidateOriginAuthorizationFilter(IOptions<ValidateOriginOptions> options)
+    {
+      this.options = options;
+    }
+
+    public void OnAuthorization(AuthorizationFilterContext context)
+    {
+      // Validate only "unsafe" methods
+      if (!ShouldValidate(context.HttpContext)) return;
+
+      // Make sure we can get the request origin
+      var requestOriginUrl = GetRequestOriginFromHeaders(context.HttpContext);
+      if (requestOriginUrl == null)
+      {
+        context.Result = new BadRequestResult();
+      }
+
+      // If it is any of the whitelisted origins, allow it (This could be more complex and allow for subdomains)
+      if (options.Value.WhitelistedOrigins.Contains(requestOriginUrl.Host))
+      {
+        return;
+      }
+
+      // Otherwise, get the target origin and make sure they match
+      var targetOriginHost = GetTargetOriginFromHeaders(context.HttpContext);
+      if (!targetOriginHost.Equals(requestOriginUrl.Host, StringComparison.CurrentCultureIgnoreCase))
+      {
+        context.Result = new BadRequestResult();
+      }
+    }
+
+    private bool ShouldValidate(HttpContext httpContext)
+    {
+      var method = httpContext.Request.Method;
+      if (string.Equals("GET", method, StringComparison.OrdinalIgnoreCase) ||
+          string.Equals("HEAD", method, StringComparison.OrdinalIgnoreCase) ||
+          string.Equals("TRACE", method, StringComparison.OrdinalIgnoreCase) ||
+          string.Equals("OPTIONS", method, StringComparison.OrdinalIgnoreCase))
+      {
+        return false;
+      }
+      return true;
+    }
+
+    private Uri GetRequestOriginFromHeaders(HttpContext httpContext)
+    {
+      // Try with the Origin header
+      var requestOrigin = httpContext.Request.Headers["Origin"];
+
+      // If not found, then try with Referer
+      if (String.IsNullOrWhiteSpace(requestOrigin))
+      {
+        requestOrigin = httpContext.Request.Headers["Referer"];
+      }
+
+      if (String.IsNullOrEmpty(requestOrigin)) return null;
+      return new Uri(requestOrigin);
+    }
+
+    private string GetTargetOriginFromHeaders(HttpContext httpContext)
+    {
+      // Try with the Host header
+      var targetOrigin = httpContext.Request.Headers["Host"];
+
+      // If not found, then try with X-Forwarded-Host
+      if (String.IsNullOrWhiteSpace(targetOrigin))
+      {
+        targetOrigin = httpContext.Request.Headers["X-Forwarded-Host"];
+      }
+
+      // If still not found, will use the request url
+      return String.IsNullOrEmpty(targetOrigin) ?
+          httpContext.Request.Host.Host :
+          new HostString(targetOrigin).Host;
+    }
+  }
+}
--- AngularReduxTest/Startup.cs
+++ AngularReduxTest/Startup.cs
@@ -7,52 +7,65 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using AngularReduxTest.Filters;
 
 namespace AngularReduxTest
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
-                {
-                    HotModuleReplacement = true
-                });
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
-
-            app.UseStaticFiles();
-
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-
-                routes.MapSpaFallbackRoute(
-                    name: "spa-fallback",
-                    defaults: new { controller = "Home", action = "Index" });
-            });
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddOptions();
+      services.Configure<ValidateOriginOptions>(Configuration);
+
+      // Add framework services.
+      services.AddAntiforgery(opts => opts.HeaderName = "X-XSRF-Token");
+      services.AddMvc(opts =>
+      {
+        opts.Filters.AddService(typeof(AngularAntiforgeryCookieResultFilter));
+        opts.Filters.AddService(typeof(ValidateOriginAuthorizationFilter));
+      });
+
+      services.AddTransient<AngularAntiforgeryCookieResultFilter>();
+      services.AddTransient<ValidateOriginAuthorizationFilter>();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+        app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
+        {
+          HotModuleReplacement = true
+        });
+      }
+      else
+      {
+        app.UseExceptionHandler("/Home/Error");
+      }
+
+      app.UseStaticFiles();
+
+      app.UseMvc(routes =>
+      {
+        routes.MapRoute(
+                  name: "default",
+                  template: "{controller=Home}/{action=Index}/{id?}");
+
+        routes.MapSpaFallbackRoute(
+                  name: "spa-fallback",
+                  defaults: new { controller = "Home", action = "Index" });
+      });
+    }
+  }
 }
